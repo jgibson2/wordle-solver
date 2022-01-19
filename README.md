437484119baab244f043b4ec7cf4ebe29f2c20c9
John Gibson <gibbyje2@gmail.com>
2022-01-19 00:03:58 -0800

    Update everything

--- README.md
+++ README.md
@@ -1,11 +1,59 @@
+# Overview
+
 This is a wordle solver. There are two modes:
 
 ```shell
-python3 main.py interactive
+python3 solver.py
 ```
 for an interactive solver and
 
 ```shell
-python3 main.py run_all
+python3 solver.py --run-mode simulate
 ```
-to simulate all possible Wordle games.
+to simulate all possible Wordle games and report the average number of guesses and win rate.
+
+# Usage
+```
+Usage: solver.py [OPTIONS]
+
+Options:
+  --run-mode [interactive|simulate]
+                                  Run in interactive mode (default) or
+                                  simulate all possible games
+  --hard-mode BOOLEAN             Run games in hard mode
+  --max-attempts INTEGER          Maximum number of attempts in a game
+  --num-guesses INTEGER           Number of guesses to check while searching
+                                  for optimum
+  --words-file PATH               File containing valid solutions and guesses
+  --first-guess TEXT              Default first guess to make. Leave blank for
+                                  random. (Warning: this will take a long
+                                  time)
+  --help                          Show this message and exit.
+```
+
+# How It Works
+
+The core idea of the system is quite simple: greedily make a guess that eliminates
+the maximal number of possibilities until only one remains. In practice, this takes several steps.
+
+### The First Guess
+The optimal first guess can be determined by searching the entire space of guesses for the one that
+eliminates the most solutions, in expectation. Because each new game is independent, this can be used
+for every game. The optimal guess is **soare**, which is a type of young hawk, but you can set the tool
+to use a different word initially, or let it choose one itself (but be warned that this will take a long time).
+
+### Subsequent Guesses
+After the first guess, the algorithm will filter out potential solutions using the information gained
+from each round:
+* Any solutions without all CORRECT letters in the proper positions are removed
+* Any solutions with letters marked ABSENT (and not PRESENT) are removed
+* Any leftover solutions without all CORRECT and PRESENT letters (in the proper multiplicities) are also removed
+
+Then, a large number of guesses are sampled from the valid guess list (default 1000) and combined with all remaining valid solutions,
+and the one that removes the largest number of additional solutions in expectation over the remaining solutions is returned as the 
+next guess. When the number of remaining solutions equals one, it is returned.
+
+# Results
+
+The algorithm averages 3.76 guesses per game in normal mode and has a win rate of 100%. In hard mode, the algorithm
+averages a slightly lower 3.70 guesses per game and has a win rate of 99.65%. 
--- solver.py
+++ solver.py
@@ -1,5 +1,6 @@
 import copy
 import functools
+import itertools
 import sys
 
 import click
@@ -13,14 +14,14 @@ import concurrent.futures
 
 
 class GuessResult(enum.Enum):
-    NOT_PRESENT = 0
+    ABSENT = 0
     PRESENT = 1
     CORRECT = 2
 
     @staticmethod
     def format_result(guess):
         replacements = {
-            GuessResult.NOT_PRESENT: '✘',
+            GuessResult.ABSENT: '✘',
             GuessResult.PRESENT: '□',
             GuessResult.CORRECT: '✔',
         }
@@ -33,27 +34,28 @@ class Outcome(enum.Enum):
 
 
 class Wordle:
-    def __init__(self, solution, valid_solutions, valid_guesses, max_guesses=6):
+    def __init__(self, solution, valid_solutions, valid_guesses, max_attempts=6):
         self.solution = solution
         if solution not in valid_solutions:
             raise ValueError("solution not in valid solutions")
         self.valid_guesses = set(valid_guesses)
-        self.max_guesses = max_guesses
-        self.num_guesses = 0
+        self.valid_solutions = set(valid_solutions)
+        self.max_attempts = max_attempts
+        self.num_attempts = 0
 
     def guess(self, guess):
-        if self.num_guesses > self.max_guesses:
+        if self.num_attempts > self.max_attempts:
             raise ValueError("guesses exceeds max guesses")
         if len(guess) != len(self.solution):
             raise ValueError("length of guess and solution do not match")
-        if guess not in self.valid_guesses:
+        if guess not in self.valid_guesses and guess not in self.valid_solutions:
             raise ValueError("guess not in valid guesses")
         outcome = None
         result = Wordle.check(guess, self.solution)
-        self.num_guesses += 1
+        self.num_attempts += 1
         if all([r == GuessResult.CORRECT for r in result]):
             outcome = Outcome.WIN
-        elif self.num_guesses == self.max_guesses:
+        elif self.num_attempts == self.max_attempts:
             outcome = Outcome.LOSE
         return result, outcome
 
@@ -62,14 +64,14 @@ class Wordle:
 
     @staticmethod
     def check(guess, solution):
-        result = [GuessResult.NOT_PRESENT] * len(guess)
+        result = [GuessResult.ABSENT] * len(guess)
         current_counts = collections.Counter(list(solution))
         for i, (guess_letter, solution_letter) in enumerate(zip(list(guess), solution)):
             if guess_letter == solution_letter:
                 result[i] = GuessResult.CORRECT
                 current_counts[guess_letter] -= 1
         for i, (guess_letter, solution_letter) in enumerate(zip(list(guess), solution)):
-            if result[i] == GuessResult.NOT_PRESENT and guess_letter in current_counts and current_counts[guess_letter] > 0:
+            if result[i] == GuessResult.ABSENT and guess_letter in current_counts and current_counts[guess_letter] > 0:
                 result[i] = GuessResult.PRESENT
                 current_counts[guess_letter] -= 1
         return result
@@ -78,16 +80,17 @@ class Wordle:
 class WordleSolver:
     ALPHABET = tuple((chr(ord('a') + i) for i in range(26)))
 
-    def __init__(self, num_letters, valid_solutions, valid_guesses, cached_first_guess=None, num_guesses=1000):
+    def __init__(self, num_letters, valid_solutions, valid_guesses, cached_first_guess=None, num_guesses=1000, hard_mode=False):
         self.num_letters = num_letters
-        self.valid_solutions = copy.copy(valid_solutions)
-        self.valid_guesses = valid_guesses
+        self.valid_solutions = set(valid_solutions)
+        self.valid_guesses = set(valid_guesses)
         self.letter_sets = [set(list(WordleSolver.ALPHABET)) for _ in range(self.num_letters)]
         self.present_character_counts = {}
         self.known = [None] * self.num_letters
         self.past_guesses = []
         self.cached_guess = cached_first_guess
         self.num_guesses = num_guesses
+        self.hard_mode = hard_mode
 
     def update(self, guess, guess_result):
         guess = guess.lower()
@@ -105,6 +108,15 @@ class WordleSolver:
         ):
             self.valid_solutions.remove(soln)
 
+        if self.hard_mode:
+            for guess in self._find_invalidated_solutions(
+                    self.valid_guesses,
+                    self.known,
+                    self.letter_sets,
+                    self.present_character_counts,
+            ):
+                self.valid_guesses.remove(guess)
+
     def _update_in_place(self, guess, guess_result, known, letter_sets, present_character_counts):
         # update present counts
         present_counts = collections.Counter([g for g, r in zip(guess, guess_result) if r == GuessResult.PRESENT or r == GuessResult.CORRECT])
@@ -115,16 +127,16 @@ class WordleSolver:
                 letter_sets[i] = set()
                 known[i] = letter
         for i, (letter, result) in enumerate(zip(list(guess), guess_result)):
-            if result == GuessResult.NOT_PRESENT:
+            if result == GuessResult.ABSENT:
                 if letter not in present_character_counts:
                     for s in letter_sets:
                         if letter in s:
                             s.remove(letter)
                 elif letter in letter_sets[i]:
                     # if we guessed the same letter 2 times but there's only one,
-                    # one of them will be marked "PRESENT" while the other is
-                    # marked as "NOT PRESENT" -- even though this second one is
-                    # marked as "NOT PRESENT" here, it just means that it's not at
+                    # one of them will be marked PRESENT while the other is
+                    # marked as ABSENT -- even though this second one is
+                    # marked as ABSENT here, it just means that it's not at
                     # this location (and a second one doesn't exist)
                     # extrapolates to 3+ as well
                     letter_sets[i].remove(letter)
@@ -154,7 +166,7 @@ class WordleSolver:
         return to_remove
 
     def suggest_guess(self):
-        if len(self.past_guesses) == 0 and self.cached_guess is not None:
+        if len(self.past_guesses) == 0 and self.cached_guess is not None and len(self.cached_guess) > 0:
             return self.cached_guess
         if len(self.valid_solutions) == 1:
             return next(iter(self.valid_solutions))
@@ -177,12 +189,21 @@ class WordleSolver:
             # print(f'Guess {guess} removed {removed} solutions')
             return removed
 
-        return max(np.random.choice(self.valid_guesses, self.num_guesses), key=key)
+        return max(
+            itertools.chain(np.random.choice(tuple(self.valid_guesses), self.num_guesses), self.valid_solutions),
+            key=key)
 
 
-def run_game(valid_solutions, valid_guesses, word):
-    wordle = Wordle(word, valid_solutions, valid_guesses)
-    solver = WordleSolver(wordle.num_letters(), valid_solutions, valid_guesses, cached_first_guess="soare")
+def run_game(hard_mode, max_attempts, num_guesses, first_guess, valid_solutions, valid_guesses, word):
+    wordle = Wordle(word, valid_solutions, valid_guesses, max_attempts=max_attempts)
+    solver = WordleSolver(
+        wordle.num_letters(),
+        valid_solutions,
+        valid_guesses,
+        cached_first_guess=first_guess,
+        hard_mode=hard_mode,
+        num_guesses=num_guesses
+    )
     guesses = []
     while True:
         guess = solver.suggest_guess()
@@ -196,50 +217,80 @@ def run_game(valid_solutions, valid_guesses, word):
                 f'{outcome_str} game with word {word}; guessed {" -> ".join(guesses)}',
                 file=outcome_file
             )
-            return outcome
+            return outcome, len(guesses)
 
 
-@click.group()
-def cli():
-    pass
+def simulate(hard_mode, max_attempts, num_guesses, first_guess, valid_solutions, valid_guesses):
+    fn = functools.partial(
+        run_game,
+        hard_mode,
+        max_attempts,
+        num_guesses,
+        first_guess,
+        valid_solutions,
+        valid_guesses
+    )
 
-
-@cli.command("run_all")
-def run_all():
-    with open('words.json', 'r') as words_file:
-        words_obj = json.load(words_file)
-        valid_solutions = words_obj['solutions']
-        valid_guesses = valid_solutions + words_obj['valid_guesses']
-
-    fn = functools.partial(run_game, valid_solutions, valid_guesses)
     with concurrent.futures.ProcessPoolExecutor() as pool:
-        results = pool.map(fn, valid_solutions)
-        if all(map(lambda r: r == Outcome.WIN, results)):
-            print("WON ALL GAMES")
+        results = list(pool.map(fn, valid_solutions))
+        avg_guesses = sum((r[1] for r in results)) / len(results)
+        print(f"Used {avg_guesses:.2f} guesses on average.")
+        win_rate = len(list(filter(lambda r: r[0] == Outcome.WIN, results))) / len(results) * 100.0
+        print(f"Win rate: {win_rate:.2f}%")
 
 
-@cli.command("interactive")
-def interactive():
-    with open('words.json', 'r') as words_file:
-        words_obj = json.load(words_file)
-        valid_solutions = words_obj['solutions']
-        valid_guesses = valid_solutions + words_obj['valid_guesses']
-    solver = WordleSolver(5, valid_solutions, valid_guesses, cached_first_guess="soare")
+def interactive(hard_mode, max_attempts, num_guesses, first_guess, valid_solutions, valid_guesses):
+    num_letters = len(next(iter(valid_solutions)))
+    solver = WordleSolver(
+        num_letters,
+        valid_solutions,
+        valid_guesses,
+        cached_first_guess=first_guess,
+        hard_mode=hard_mode,
+        num_guesses=num_guesses
+    )
     replacements = {
         "g": GuessResult.CORRECT,
         "y": GuessResult.PRESENT,
-        "b": GuessResult.NOT_PRESENT,
+        "b": GuessResult.ABSENT,
     }
 
-    while True:
+    for _ in range(max_attempts):
         print(f"{len(solver.valid_solutions)} solutions remaining")
         next_guess = solver.suggest_guess()
         print(f'Suggested next guess: {next_guess.upper()}')
         guess = input("Input your guess:").strip().lower()
         results = input("Input the results (Y = yellow, G = green, B = blank):").strip().lower()
+        if results == ''.join(["g"] * num_letters):
+            print("You won!")
+            break
         guess_results = [replacements[c] for c in list(results)]
         solver.update(guess, guess_results)
 
+    print("You lost :(")
+
+
+@click.command()
+@click.option("--run-mode", type=click.Choice(["interactive", "simulate"]), default="interactive",
+              help="Run in interactive mode (default) or simulate all possible games")
+@click.option("--hard-mode", type=click.BOOL, default=False, help="Run games in hard mode")
+@click.option("--max-attempts", type=click.INT, default=6, help="Maximum number of attempts in a game")
+@click.option("--num-guesses", type=click.INT, default=1000, help="Number of guesses to check while searching for optimum")
+@click.option("--words-file", type=click.Path(), default="words.json", help="File containing valid solutions and guesses")
+@click.option("--first-guess", type=click.STRING, default="soare",
+            help="Default first guess to make. Leave blank for random. (Warning: this will take a long time)")
+def main(run_mode, hard_mode, max_attempts, num_guesses, words_file, first_guess):
+    with open(words_file, 'r') as wf:
+        words_obj = json.load(wf)
+        valid_solutions = words_obj['valid_solutions']
+        valid_guesses = words_obj['valid_guesses']
+    if run_mode == "interactive":
+        interactive(hard_mode, max_attempts, num_guesses, first_guess, valid_solutions, valid_guesses)
+    elif run_mode == "simulate":
+        simulate(hard_mode, max_attempts, num_guesses, first_guess, valid_solutions, valid_guesses)
+    else:
+        raise ValueError("Invalid run mode!")
+
 
 if __name__ == '__main__':
-    cli()
+    main()
